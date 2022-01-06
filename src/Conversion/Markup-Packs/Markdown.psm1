d36236e645d7656425e15efb2de16a24e27193aa
Antonio Lopez Rivera <antonlopezr99@gmail.com>
2022-01-06 16:25:49 +0100

    MarkdownPack1 v1

--- src/Conversion/Markup-Packs/Markdown.psm1
+++ src/Conversion/Markup-Packs/Markdown.psm1
@@ -1,10 +1,10 @@
 # SPDX-License-Identifier: GPL-3.0
 
-Function Format-MarkDown
+Function MarkdownPack1
 {
     [CmdletBinding()]
     param (
-        # Dryrun trigger
+        # OneUp configuration object
         [Parameter(Mandatory)]
         [object]
         $config
@@ -15,111 +15,131 @@ Function Format-MarkDown
         $pageCfg
     )
 
-    # Pandoc Markdown output formatting. Each search and replace is done against a string containing the entire markdown content
+    # Markup output formatting using search and replace queries against a string containing the entire markdown content.
 
-    foreach ($attachmentCfg in $pageCfg['insertedAttachments']) {
-        @{
-            description = 'Change inserted attachment(s) filename references'
-            replacements = @(
-                @{
-                    searchRegex = [regex]::Escape( $attachmentCfg['object'].preferredName )
-                    replacement = "[$( $attachmentCfg['markdownFileName'] )]($( $pageCfg['mediaPathPandoc'] )/$( $attachmentCfg['markdownFileName'] ))"
-                }
-            )
-        }
-    }
-    @{
-        description = 'Replace media (e.g. images, attachments) absolute paths with relative paths'
-        replacements = @(
+    $markupPack = @(
+        foreach ($attachmentCfg in $pageCfg['insertedAttachments']) {
             @{
-                # E.g. 'C:/temp/notes/mynotebook/media/somepage-image1-timestamp.jpg' -> '../media/somepage-image1-timestamp.jpg'
-                searchRegex = [regex]::Escape("$( $pageCfg['mediaParentPathPandoc'] )/") # Add a trailing front slash
-                replacement = $pageCfg['levelsPrefix']
-            }
-        )
-    }
-    @{
-        description = 'Add heading'
-        replacements = @(
-            @{
-                searchRegex = '^[^\r\n]*'
-                replacement = & {
-                    $heading = "# $( $pageCfg['object'].name )"
-                    if ($config['headerTimestampEnabled']['value'] -eq 1) {
-                        $heading += "`n`nCreated: $(  $pageCfg['dateTime'].ToString('yyyy-MM-dd HH:mm:ss zz00') )"
-                        $heading += "`n`nModified: $(  $pageCfg['lastModifiedTime'].ToString('yyyy-MM-dd HH:mm:ss zz00') )"
-                        $heading += "`n`n---`n"
-                    }
-                    $heading
-                }
-            }
-        )
-    }
-    if ($config['keepspaces']['value'] -eq 1 ) {
-        @{
-            description = 'Clear double spaces from bullets and non-breaking spaces spaces from blank lines'
-            replacements = @(
-                @{
-                    searchRegex = [regex]::Escape([char]0x00A0)
-                    replacement = ''
-                }
-                # Remove a newline between each occurrence of '- some list item'
-                @{
-                    searchRegex = '\r*\n\r*\n- '
-                    replacement = "`n- "
-                }
-                # Remove all '>' occurrences immediately following bullet lists
-                @{
-                    searchRegex = '\n>[ ]*'
-                    replacement = "`n"
-                }
-            )
-        }
-    }
-    if ($config['keepescape']['value'] -eq 1) {
-        @{
-            description = "Clear all '\' characters"
-            replacements = @(
-                @{
-                    searchRegex = [regex]::Escape('\')
-                    replacement = ''
-                }
-            )
-        }
-    }
-    elseif ($config['keepescape']['value'] -eq 2) {
-        @{
-            description = "Clear all '\' characters except those preceding alphanumeric characters"
-            replacements = @(
-                @{
-                    searchRegex = '\\([^A-Za-z0-9])'
-                    replacement = '$1'
-                }
-            )
-        }
-    }
-    & {
-        if ($config['newlineCharacter']['value'] -eq 1) {
-            @{
-                description = "Use LF for newlines"
+                description = 'Change inserted attachment(s) filename references'
                 replacements = @(
                     @{
-                        searchRegex = '\r*\n'
+                        searchRegex = [regex]::Escape( $attachmentCfg['object'].preferredName )
+                        replacement = "[$( $attachmentCfg['markdownFileName'] )]($( $pageCfg['mediaPathPandoc'] )/$( $attachmentCfg['markdownFileName'] ))"
+                    }
+                )
+            }
+        }
+        @{
+            description = 'Replace media (e.g. images, attachments) absolute paths with relative paths'
+            replacements = @(
+                @{
+                    # E.g. 'C:/temp/notes/mynotebook/media/somepage-image1-timestamp.jpg' -> '../media/somepage-image1-timestamp.jpg'
+                    searchRegex = [regex]::Escape("$( $pageCfg['mediaParentPathPandoc'] )/") # Add a trailing front slash
+                    replacement = $pageCfg['levelsPrefix']
+                }
+            )
+        }
+        @{
+            description = 'Add heading'
+            replacements = @(
+                @{
+                    searchRegex = '^[^\r\n]*'
+                    replacement = & {
+                        $heading       = "# $( $pageCfg['object'].name )"
+                        $creationDate  = $pageCfg['lastModifiedTime']
+                        $creationDay   = $creationDate.DayOfWeek.ToString()
+                        $creationMonth = (Get-Culture).DateTimeFormat.GetMonthName([int]$pageCfg['lastModifiedTime'].ToString('MM'))
+                        if ($config['headerTimestampEnabled']['value'] -eq 1) {
+                            $heading += "`n`n$( $creationDay ), $( $creationMonth ) $( $pageCfg['lastModifiedTime'].ToString('d, yyyy') ) $('`')$( $pageCfg['lastModifiedTime'].ToString('HH:mm') )$('`')"
+                            $heading += "`n`n---"
+                        }
+                        $heading
+                    }
+                }
+            )
+        }
+        @{
+            description = 'Remove OneNote export artifacts'
+            replacements = @(
+                @{
+                    searchRegex = "\n\n$( [char]0x00C2 )"
+                    replacement = ""
+                }
+            )
+        }
+        if ($config['keepspaces']['value'] -eq 1 ) {
+            @{
+                description = 'Clear double spaces from bullets and non-breaking spaces spaces from blank lines'
+                replacements = @(
+                    @{
+                        searchRegex = [regex]::Escape([char]0x00A0)
+                        replacement = ''
+                    }
+                    # Remove double newlines before unnumbered list items
+                    @{
+                        searchRegex = '(?<=\n- .*?)(\n)(?=\n-)'
+                        replacement = ""
+                    }
+                    # Remove double newlines before numbered list items
+                    @{
+                        searchRegex = '(?<=\n[0-9]+. .*?)(\n)(?=\n[0-9]+.)'
+                        replacement = ""
+                    }
+                    # Remove all '>' occurrences immediately following bullet lists
+                    @{
+                        searchRegex = '\n>[ ]*'
                         replacement = "`n"
                     }
                 )
             }
-        }else {
+        }
+        if ($config['keepescape']['value'] -eq 1) {
             @{
-                description = "Use CRLF for newlines"
+                description = "Clear all '\' characters"
                 replacements = @(
                     @{
-                        searchRegex = '\r*\n'
-                        replacement = "`r`n"
+                        searchRegex = [regex]::Escape('\')
+                        replacement = ''
                     }
                 )
             }
         }
-    }
+        elseif ($config['keepescape']['value'] -eq 2) {
+            @{
+                description = "Clear all '\' characters except those preceding alphanumeric characters"
+                replacements = @(
+                    @{
+                        searchRegex = '\\([^A-Za-z0-9])'
+                        replacement = '$1'
+                    }
+                )
+            }
+        }
+        & {
+            if ($config['newlineCharacter']['value'] -eq 1) {
+                @{
+                    description = "Use LF for newlines"
+                    replacements = @(
+                        @{
+                            searchRegex = '\r*\n'
+                            replacement = "`n"
+                        }
+                    )
+                }
+            }else {
+                @{
+                    description = "Use CRLF for newlines"
+                    replacements = @(
+                        @{
+                            searchRegex = '\r*\n'
+                            replacement = "`r`n"
+                        }
+                    )
+                }
+            }
+        }
+    )
+
+    $markupPack
 
 }
